5b4b6b7227d26c603e2e9fccc5022b4a18750ce6
Markus Aleksander Råkil Johansen <markusrj@stud.ntnu.no>
2021-10-20 20:05:55 +0200

    structural comments in css and script, also changed promillekalkulator structure.

--- ontappd/js/script.js
+++ ontappd/js/script.js
@@ -1,5 +1,6 @@
+//--------------------------------------------------
 //Promille kalkulator
-
+//--------------------------------------------------
 let kvinne = document.getElementById("kvinne");
 let mann = document.getElementById("mann");
 let vekt = document.getElementById("kroppsvekt")
@@ -7,32 +8,57 @@ let alkohol = document.getElementById("enheter")
 let drikkestart = document.getElementById("drikkestart")
 let prom = document.getElementById("p_output")
 
-function promille(){
-    console.log("promillekalkulator starter..")
+function check_if_zero_print(value,output){//bestemmer output avhengig av om input <= 0 eller input > 0
+    if(value <= 0){
+        output.innerHTML = "Promille = 0";//Dersom promillen er 0 eller mindre utifra formelen er minste mulige verdi av promille i blodet 0 eller tilnærmet null og derfor setter vi verdien til 0 i dette intervallet.
+    }
+    else{
+        output.innerHTML = "Promille = " + parseFloat(value).toFixed(3); //viser promille verdien på nettsiden, med opptil tre decimaler.
+    }
+    console.log("promille = " + p);//logger den faktiske promille verdien i konsollen.
+}
+function BAC_calculation(alcohol_by_units, weight, hours_from_start, output, female_button, male_button){//beregningene
     let x = 0;
 
-    if(kvinne.checked){
-        x = (parseFloat(alkohol.value)* 12.8 )/(parseFloat(vekt.value) * 0.60); // 12.8g alkohol/per enhet * antall enheter = antall gram alkohol
-
-    }else if(mann.checked){
-        x = (parseFloat(alkohol.value)* 12.8 )/(parseFloat(vekt.value) * 0.70); 
-
+    if(female_button.checked){
+        x = (parseFloat(alcohol_by_units.value)* 12.8 )/(parseFloat(weight.value) * 0.60); // 12.8g alkohol/per enhet * antall enheter = antall gram alkohol
+    }else if(male_button.checked){
+        x = (parseFloat(alcohol_by_units.value)* 12.8 )/(parseFloat(weight.value) * 0.70); 
     }else{
         console.log("No gender picked...");
-        prom.innerHTML = "No gender picked. Try again, pick a gender.";
+        output.innerHTML = "No gender picked. Try again, pick a gender.";
         return false
     }
 
-    let y = 0.15 * parseFloat(drikkestart.value);
+    let y = 0.15 * parseFloat(hours_from_start.value);
     console.log("y = " + y)
-    let p = x - y; //følger formel fra NHI: Alkohol i gram / (kroppsvekten i kg x 0.70) - (0,15 x timer fra drikkestart) = promille
-    
-    if(p <= 0){
-        prom.innerHTML = "Promille = 0";
-    }
-    else{
-        prom.innerHTML = "Promille = " + p;
-    }
-    console.log("p = " + p);
-
+    return promille = x - y;
 }
+
+function promille(){//funksjonen som får et kall fra submit knappen. Main funksjon for kalkulatoren.
+    console.log("promillekalkulator starter..")
+    BAC_calculation(alkohol, vekt, drikkestart, prom, kvinne, mann)
+    check_if_zero_print(promille,prom);
+}
+/*BUGS:
+-Knapp refresher ikke resultatet, du kan ikke endre uten reload
+-trykker du inn gender og submitter får du promille NaN. gjør alle felt required
+*/
+
+
+//--------------------------------------------------
+//Spleise kalkulator
+//--------------------------------------------------
+
+//--------------------------------------------------
+//ABV kalkulator
+//--------------------------------------------------
+
+//--------------------------------------------------
+//Navbar
+//--------------------------------------------------
+
+
+//--------------------------------------------------
+//Picture slideshow
+//--------------------------------------------------

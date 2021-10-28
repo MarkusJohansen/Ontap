74552e8360742132ae0353774e32a38da0d8f5cd
Benjamin Fogalele <bcfogale@stud.ntnu.no>
2021-10-28 15:17:21 +0200

    Prøver fortsatt å fikse liste

--- ontappd/js/liste.js
+++ ontappd/js/liste.js
@@ -1,15 +1,16 @@
 const ul = document.getElementById("tutorial_list");
 const li = document.createElement("li")
 const tutorial = ["a", "b", "c", "d", "e"];
+const span = document.createElement("span")
 const checkbox = document.createElement("input");
 checkbox.type = "checkbox";
 
 
-
 for (let i = 0; i < tutorial.length; i++) {
-    
-    li.appendChild.checkbox
-    ul.appendChild.li
-    li.innerHTML = tutorial[i]
+    li.appendChild(checkbox)
+    ul.appendChild(li)
+    li.appendChild(span)
+    span.appendChild(document.createTextNode(tutorial[i]))
 }
+
 

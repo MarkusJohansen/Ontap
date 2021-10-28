e2d7892a193c87e14b4a63147b110461ed12fae1
Benjamin Fogalele <bcfogale@stud.ntnu.no>
2021-10-28 16:12:56 +0200

    Slettet min JS fil og importerte innholdet til howto.js

---
+++ ontappd/js/components/howTo.js
@@ -0,0 +1,20 @@
+
+
+function howtoliste() {
+    const ul = document.getElementById("tutorial_list");
+    const li = document.createElement("li")
+    const tutorial = ["a", "b", "c", "d", "e"];
+    const span = document.createElement("span")
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.class = "checkbox"
+
+
+    for (let i = 0; i < tutorial.length; i++) {
+        li.appendChild(checkbox)
+        ul.appendChild(li)
+        li.appendChild(span)
+        span.appendChild(document.createTextNode(tutorial[i]))
+    }
+
+}

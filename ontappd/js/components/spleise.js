357a03797a134f0d6bb7f2247f45e3eeea54a433
Naqibullah Rahimi <naqibulr@stud.ntnu.no>
2021-10-28 10:44:14 +0000

    Update spleise.js

---
+++ ontappd/js/components/spleise.js
@@ -0,0 +1,23 @@
+const antallPersoner = document.getElementById("antallPersoner")
+const antallLiter = document.getElementById("antallLiter")
+const totalKostnad = document.getElementById("totalKostnad")
+const submit = document.getElementById("submit")
+const output1 = document.getElementById("literPerPers")
+const output2 = document.getElementById("kronerPerPers")
+const output3 = document.getElementById("kronerPerLiter")
+
+
+submit.addEventListener("click",function(event){
+    const antallPersonerValue = antallPersoner.value
+    const antallLiterValue = antallLiter.value 
+    const totalKostnadValue = totalKostnad.value
+    const literPerPerson = antallLiterValue/antallPersonerValue
+    const kronerPerPerson = totalKostnadValue/antallPersonerValue
+    const kronerPerLiter = totalKostnadValue/antallLiterValue
+    output1.value= literPerPerson
+    output2.value= kronerPerPerson
+    output3.value= kronerPerLiter
+    event.preventDefault()
+
+    
+})

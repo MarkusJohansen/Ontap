0c4e3c40d6e7687af1103a3e763438a2165a63e9
Benjamin Fogalele <bcfogale@stud.ntnu.no>
2021-10-28 13:27:22 +0200

    Har begynt med scriptet til tutorialen/listen

---
+++ ontappd/js/liste.js
@@ -0,0 +1,12 @@
+const li = document.createElement("li");
+const tutorial = [""];
+const checkbox = document.createElement("input");
+task1.type = "checkbox";
+
+
+let text = "";
+function brygge_liste() {
+    for (let i = 0; i < 6; i++) {
+
+    }
+}

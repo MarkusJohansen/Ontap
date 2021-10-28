2f3e25fda9600edc16bd05831ff586db8c24cbe1
Oskar Nesheim <oskarin@stud.ntnu.no>
2021-10-28 12:22:30 +0200

    28.10 waste slideshow

--- ontappd/js/script.js
+++ ontappd/js/script.js
@@ -61,7 +61,14 @@ const output_abv = document.getElementById("output_abv");
 
 function ABV_calculator(){
     const abv = ((starting_gravity.value)-(end_gravity.value))*131.25;
-    output_abv.value = abv;
+    const insufficient_values = "Uyldig verdi"
+    if (abv < 0){
+        output_abv.value = insufficient_values;
+    }
+    else {
+        output_abv.value = abv;
+    }
+    
 }
 
 //--------------------------------------------------

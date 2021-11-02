3690b73c5e7978a119712276d741bbfc3f23bb82
Oskar Nesheim <oskarin@stud.ntnu.no>
2021-11-02 16:08:22 +0100

    kontaktside

--- ontappd/js/components/kontakside.js
+++ ontappd/js/components/kontakside.js
@@ -1,10 +1,12 @@
-const name = document.querySelector("#form_name");
-const mail = document.querySelector("#form_mail");
-const subject = document.querySelector("#form_subject");
-const message = document.querySelector("#form_message");
-const submit = document.querySelector("#form_submit");
+function sendEmail(){
+    const name = document.querySelector("#form_name");
+    const mail = document.querySelector("#form_mail");
+    const subject = document.querySelector("#form_subject");
+    const message = document.querySelector("#form_message");
+    const submit = document.querySelector("#form_submit");
 
-function sendEmail(e){
-    e.preventDefault();
-    window.open('mailto:Ontappd@gmail.com?subject=subject&body=body');
+    var body = encodeURI(message)+"%0D%0A"+"%0D%0A"+encodeURI(name)+"%0D%0A"+encodeURI(subject);
+    window.location.href='mailto:' +'Ontappd@gmail.com'+'?subject='+encodeURI(subject)+'&body='+body;
+    return false;
 }
+

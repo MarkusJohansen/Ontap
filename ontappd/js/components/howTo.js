9525e3f93eb07e6f3f0d7adbc5fac0fdcb094b47
Benjamin Fogalele <bcfogale@stud.ntnu.no>
2021-11-02 14:15:08 +0100

    Pusher før merging av branch

--- ontappd/js/components/howTo.js
+++ ontappd/js/components/howTo.js
@@ -1,20 +1,17 @@
 
-
-function howtoliste() {
-    const ul = document.getElementById("tutorial_list");
-    const li = document.createElement("li")
-    const tutorial = ["a", "b", "c", "d", "e"];
-    const span = document.createElement("span")
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
+const ul = document.getElementById("tutorial_list");
+const li = document.createElement("li")
+const tutorial = ["a", "b", "c", "d", "e"];
+const span = document.createElement("span")
+const checkbox = document.createElement("input");
+checkbox.type = "checkbox";
     
 
 
-    for (let i = 0; i < tutorial.length; i++) {
-        li.appendChild(checkbox)
-        ul.appendChild(li)
-        li.appendChild(span)
-        span.appendChild(document.createTextNode(tutorial[i]))
-    }
-
+for (let i = 0; i < tutorial.length; i++) {
+    ul.appendChild(li)
+    li.appendChild(checkbox)
+    li.appendChild(span)
+    span.innerHTML(tutorial[i])
 }
+

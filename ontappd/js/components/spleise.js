0bba9251b270ee3c90d644a0147008f1fef830e1
Naqibullah Rahimi <naqibulr@stud.ntnu.no>
2021-11-04 15:26:20 +0100

    fiksa noen feil meldinger

--- ontappd/js/components/spleise.js
+++ ontappd/js/components/spleise.js
@@ -10,11 +10,7 @@ function spleisKalkulator(){
     const antallPersonerValue = antallPersoner.value
     const antallLiterValue = antallLiter.value 
     const totalKostnadValue = totalKostnad.value
-<<<<<<< HEAD
-    if (antallPersonerValue=="" || antallPersonerValue<1 ){
-=======
     if (antallPersonerValue<1 ){
->>>>>>> 6d876da3c3da93a8e00fcee59cc233afc417303f
         document.getElementById("span1").innerText="* Tallet må være 1 eller større "
         document.getElementById("span1").style.display="inline"
     }
@@ -22,26 +18,16 @@ function spleisKalkulator(){
         document.getElementById("span1").style.display="none"
 
     }
-<<<<<<< HEAD
-    if (antallLiterValue=="" || antallLiterValue<1 ){
-        document.getElementById("span2").innerText="*Tallet må være 1 eller større"
-=======
     if (antallLiterValue<1 ){
         document.getElementById("span2").innerText="*Tallet må være 1 eller større "
->>>>>>> 6d876da3c3da93a8e00fcee59cc233afc417303f
         document.getElementById("span2").style.display="inline"
     }
     else{
         document.getElementById("span2").style.display="none"
     }
 
-<<<<<<< HEAD
-    if (totalKostnadValue=="" || totalKostnadValue <1 ){
-        document.getElementById("span3").innerText="*Tallet må være 1 eller større"
-=======
     if ( totalKostnadValue <1 ){
         document.getElementById("span3").innerText="*Tallet må være 1 eller større "
->>>>>>> 6d876da3c3da93a8e00fcee59cc233afc417303f
         document.getElementById("span3").style.display="inline"
     }
     else{

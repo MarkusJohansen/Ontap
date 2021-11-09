ebcab69459dd44e45f19dd8314006b482dd1c901
Benjamin Fogalele <bcfogale@stud.ntnu.no>
2021-11-09 01:11:31 +0100

    Har validert html og JS for howto.

--- ontappd/js/components/howTo.js
+++ ontappd/js/components/howTo.js
@@ -4,29 +4,31 @@ const tutorial = ["Lage eller kjøpe ferdig maltet korn.", "Putte kornet i brygg
  "Gjære ølen i sterilisert ballong.", "Overføre ølen til flasker og tilsette sukker for karbonering.", "Flaske ølen vha. en flaskemaskin.", "Vaske brukt utstyr."];    
 
 for (let i = 0; i < tutorial.length; i++) {
-    let li = document.createElement("li")
+    let li = document.createElement("li");
 
-    let span = document.createElement("span")
-    span.innerHTML = tutorial[i]
+    let span = document.createElement("span");
+    span.innerHTML = tutorial[i];
     span.className = "list_span";
 
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    checkbox.className = "checkboxes"
-    checkbox.setAttribute("onclick", "checked_boxes()")
+    checkbox.className = "checkboxes";
+    checkbox.setAttribute("onclick", "checked_boxes()");
 
-    li.appendChild(checkbox)
-    li.appendChild(span)
-    ul.appendChild(li)
+    li.appendChild(checkbox);
+    li.appendChild(span);
+    ul.appendChild(li);
 }
 
 
-const antallsjekket = document.getElementById("antallchecked")
-antallsjekket.innerHTML = "0/7 steg gjennomført"
+const antallsjekket = document.getElementById("antallchecked");
+antallsjekket.innerHTML = "0/7 steg gjennomført";
 
 
 function checked_boxes() {
-    let allebokser = document.querySelectorAll("input[type=checkbox]")
-    let avhuketbokser = document.querySelectorAll("input[type=checkbox]:checked")
-    antallsjekket.innerHTML = avhuketbokser.length + "/" + allebokser.length + " steg gjennomført"
+    let allebokser = document.querySelectorAll("input[type=checkbox]");
+    let avhuketbokser = document.querySelectorAll("input[type=checkbox]:checked");
+    antallsjekket.innerHTML = avhuketbokser.length + "/" + allebokser.length + " steg gjennomført";
 }
+
+document.addEventListener("onclick", checked_boxes());

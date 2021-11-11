10fd284e48493367c718c968333b3dff4be3a72e
Oskar Nesheim <oskarin@stud.ntnu.no>
2021-11-11 12:20:28 +0100

    navbar change

--- ontappd/js/script.js
+++ ontappd/js/script.js
@@ -83,10 +83,10 @@ function navbar_component(){
     //putting the navbuttons in a matrice, where every list is a button that contains properties: ["document to link to","text in button"]
     const nav_buttons = [
         ["index.html", "Hjem"],
+        ["howto.html","Brygge selv"],
+        ["spleise.html","Spleisekalkulator"],
         ["abv.html","ABV-kalkulator"],
         ["promille.html","Promillekalkulator"],
-        ["spleise.html","Spleisekalkulator"],
-        ["howto.html","Brygge selv"],
         ["contactus.html","Kontakt oss"]
     ];
     
